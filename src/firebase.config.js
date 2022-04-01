83d0ebfc476986ed1d72eac41bbb1183089a3060
sammydev <sbee1796@gmail.com>
2022-04-01 20:21:22 +0200

    Added firebase configuration to project

---
+++ src/firebase.config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFireStore} from "firebase/fireStore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDDMtEdvRh7VKimGQhPBdeFz_b862b0_pw",
+  authDomain: "marketline-housing.firebaseapp.com",
+  projectId: "marketline-housing",
+  storageBucket: "marketline-housing.appspot.com",
+  messagingSenderId: "35020354652",
+  appId: "1:35020354652:web:c17ff1405681ae0200e299",
+  measurementId: "G-TDRS2NNMP8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFireStore() 
